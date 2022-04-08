995ab8e43ca51e8d21de76e80ab90f3e9580c60e
jiho lee <optional.int@kakao.com>
2022-04-09 01:35:39 +0900

    feat: write RouterDatum type draft

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -1,7 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export type RouterDatum = {};
+// It is draft. every property coule be edited, removed or appended.
+export type RouterDatum = {
+    uri: string;
+    name: string;
+    description?: string;
+    category?: string;
+};
 
 export type RouterData = RouterDatum[];
 
