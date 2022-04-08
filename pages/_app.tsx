5b73a1c779ed4c4269fd437aadaffd4c0729d1e5
jiho lee <optional.int@kakao.com>
2022-04-09 01:18:27 +0900

    fix: tailwind not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import type { AppProps } from "next/app";
-import Layout from "../components/layout";
+import type { AppProps } from 'next/app';
+import Layout from '../components/layout';
+import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Layout>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 };
 
 export default MyApp;
