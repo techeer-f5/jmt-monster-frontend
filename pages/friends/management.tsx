991889c38a2ebc1f92edae0911bfaf917ff8e42d
jiho lee <optional.int@kakao.com>
2022-04-10 00:22:14 +0900

    feat: add dummy components

---
+++ pages/friends/management.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+const Management: NextPage = () => (
+    <Typography variant="h1" className="text-white">
+        Hello, JMT Monster!
+    </Typography>
+);
+
+export default Management;
---
+++ pages/maps/mine.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+const Mine: NextPage = () => (
+    <Typography variant="h1" className="text-white">
+        Hello, JMT Monster!
+    </Typography>
+);
+
+export default Mine;
