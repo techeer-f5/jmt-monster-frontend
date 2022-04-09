bdd56dfd156242dc78f993008f7915e2c5b0ab9b
jiho lee <optional.int@kakao.com>
2022-04-09 23:38:26 +0900

    feat: add routes API

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -9,9 +9,34 @@ export type RouterDatum = {
 
 export type RouterData = RouterDatum[];
 
-export default function handler(
+export default function getRoutes(
     req: NextApiRequest,
     res: NextApiResponse<RouterData>
 ) {
-    res.status(200).json([]); // TODO: inject router data
+    res.status(200).json([
+        {
+            uri: 'maps/mine',
+            name: '내 지도'
+        },
+        {
+            uri: 'maps/friends',
+            name: '친구 지도'
+        },
+        {
+            uri: 'maps',
+            name: '지도 목록'
+        },
+        {
+            uri: 'friends/management',
+            name: '친구 관리'
+        },
+        {
+            uri: 'profile',
+            name: '프로필'
+        },
+        {
+            uri: 'settings',
+            name: '설정'
+        }
+    ]);
 }
