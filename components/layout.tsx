091cb4b4308b0c938a2665f52b1326ca276bde46
jiho lee <optional.int@kakao.com>
2022-04-10 18:54:29 +0900

    feat: increase header text size to lg

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,7 +11,7 @@ export interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
     return (
         <div className="bg-slate-200 min-w-screen min-h-screen flex flex-grow flex-col">
-            <Header className="text-white bg-sky-300 h-[7.5vh] center-children">
+            <Header className="text-white text-lg bg-sky-300 h-[7.5vh] center-children">
                 <p>Hi Header!</p>
             </Header>
             <main className="flex flex-[1] bg-sky-600 justify-center align-middle center-children">
