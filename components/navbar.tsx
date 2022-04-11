9bc26e8f8bb17096f6131cd2be30de0f67282447
jiho lee <optional.int@kakao.com>
2022-04-11 15:19:45 +0900

    chore: use route.name directly with destructuring

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -64,7 +64,7 @@ const Navbar = () => {
                                     icon ? 'mt-0' : 'mt-auto'
                                 }`}
                             >
-                                {route.name}
+                                {name}
                             </div>
                         </div>
                     </Link>
