5420e440df81958960d118d36380dff52508873c
jiho lee <optional.int@kakao.com>
2022-04-11 21:42:48 +0900

    feat: add header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,17 @@
-import Header from './empty-div';
+import { useMemo } from 'react';
+import useMapHeader from '../store/map';
 
+const Header = () => {
+    const { title, location } = useMapHeader();
+
+    const fullTitle = useMemo(() => `${title} ${location}`, [title, location]);
+
+    return (
+        <div className="flex w-screen bg-fuchsia-500 h-[7.5vh]">
+            <div className="mx-auto my-auto flex-1 text-white text-black-border justify-center align-middle center-children text-3xl font-bold">
+                {fullTitle}
+            </div>
+        </div>
+    );
+};
 export default Header;
--- components/layout.tsx
+++ components/layout.tsx
@@ -17,9 +17,7 @@ const Layout = ({ children }: LayoutProps) => {
         <div>
             <MetaConfigs />
             <div className="bg-slate-200 min-w-screen min-h-screen flex flex-grow flex-col">
-                <Header className="text-white text-lg bg-sky-300 h-[7.5vh] center-children">
-                    <p>Hi Header!</p>
-                </Header>
+                <Header />
                 <main className="flex flex-[1] bg-sky-600 justify-center align-middle center-children">
                     <div>{children}</div>
                 </main>
