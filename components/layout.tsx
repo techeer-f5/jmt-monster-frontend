fa392da29254ed0d662aef4a305aed55c88440fa
jiho lee <optional.int@kakao.com>
2022-04-11 15:19:26 +0900

    feat: add meta tag configs

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from 'react';
 import Header from './header';
 import Navbar from './navbar';
+import MetaConfigs from './meta-configs';
+import usePwaInstall from '../hooks/use-pwa-install';
 
 export type ChildElement = ReactNode;
 
@@ -9,15 +11,20 @@ export interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+    usePwaInstall();
+
     return (
-        <div className="bg-slate-200 min-w-screen min-h-screen flex flex-grow flex-col">
-            <Header className="text-white text-lg bg-sky-300 h-[7.5vh] center-children">
-                <p>Hi Header!</p>
-            </Header>
-            <main className="flex flex-[1] bg-sky-600 justify-center align-middle center-children">
-                <div>{children}</div>
-            </main>
-            <Navbar />
+        <div>
+            <MetaConfigs />
+            <div className="bg-slate-200 min-w-screen min-h-screen flex flex-grow flex-col">
+                <Header className="text-white text-lg bg-sky-300 h-[7.5vh] center-children">
+                    <p>Hi Header!</p>
+                </Header>
+                <main className="flex flex-[1] bg-sky-600 justify-center align-middle center-children">
+                    <div>{children}</div>
+                </main>
+                <Navbar />
+            </div>
         </div>
     );
 };
