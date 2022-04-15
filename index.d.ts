74ebc468c4998bbf2af7d466a082a4b75120e574
jiho lee <optional.int@kakao.com>
2022-04-16 01:39:23 +0900

    feat: add useAuth api calls

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,5 @@
 export type Icons = keyof typeof import('@mui/icons-material');
+
+export interface TokenValidation {
+    success: boolean;
+}
--- store/auth.ts
+++ store/auth.ts
@@ -1,4 +1,6 @@
 import create from 'zustand';
+import qs from 'qs';
+import { fetchRemotes } from '../pages/api/remotes';
 
 export interface User {
     id: string;
@@ -9,16 +11,109 @@ export interface User {
 export interface AuthState {
     user: User | null;
     token: string | null;
-    provider: 'kakao' | 'google' | null;
+    generateToken: (code: string) => Promise<string>;
+    validateToken: (token: string) => Promise<boolean>;
+    fetchUserInfo: () => Promise<boolean>;
     signOut: () => Promise<void>;
 }
 
-const useAuth = create<AuthState>((set) => ({
+export interface TokenValid {
+    success: true;
+}
+
+export interface GeneratedToken {
+    // FIXME: change key name with backend
+    id: string;
+}
+
+export interface UserInfo {
+    success: boolean;
+    user?: User;
+}
+
+const useAuth = create<AuthState>((set, get) => ({
     user: null,
     token: null,
-    provider: null,
-    // TODO: implement sign out with social login
-    // eslint-disable-next-line @typescript-eslint/require-await
+    generateToken: async (code: string) => {
+        // FIXME: Redundant code repetition
+        // FIXME: Violates DRY
+        const { backend } = await fetchRemotes();
+
+        const url = `${backend}/auth/kakao/callback`;
+        const params = qs.stringify({
+            code
+        });
+
+        const generatedToken = (await (
+            await fetch(url + params, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+        ).json()) as GeneratedToken;
+
+        const { id: token } = generatedToken;
+
+        return token;
+    },
+    validateToken: async (token: string) => {
+        const { backend } = await fetchRemotes();
+
+        const { success } = (await (
+            await fetch(`${backend}/token/validate`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({ token })
+            })
+        ).json()) as TokenValid;
+
+        if (success) {
+            set({
+                token
+            });
+        }
+
+        return success;
+    },
+    fetchUserInfo: async () => {
+        const { backend } = await fetchRemotes();
+        const { token } = get();
+
+        if (!token) {
+            return false;
+        }
+
+        // TODO: API Integration
+        // FIXME: does nothing
+
+        const userResponse = (await (
+            await fetch(`${backend}/users/me`, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+        ).json()) as UserInfo;
+
+        let { success } = userResponse;
+        const { user } = userResponse;
+
+        success = success && !!user;
+
+        // Typescript does not remove undefined type with above statement automatically..
+        if (success && user) {
+            set({
+                user
+            });
+        }
+
+        return success;
+    },
+    // TODO: implement signout with social login
     signOut: async () => set({ user: null, token: null })
 }));
 
