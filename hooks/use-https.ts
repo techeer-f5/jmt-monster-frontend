6944a4e956f3998aa0cfb4ff75e4a9de7a39e413
jiho lee <optional.int@kakao.com>
2022-05-04 12:10:17 +0900

    fix: use hostname instead of origin

--- hooks/use-https.ts
+++ hooks/use-https.ts
@@ -2,16 +2,22 @@ import { useEffect } from 'react';
 
 const useHttps = () => {
     useEffect(() => {
-        const { origin, protocol } = window.location;
+        const { hostname, protocol } = window.location;
 
-        if (origin.startsWith('localhost') || origin.startsWith('127.0.0.1')) {
-            // eslint-disable-next-line no-empty
-        } else if (protocol.startsWith('http')) {
-            window.location.href = window.location.href.replace(
-                'http',
-                'https'
-            );
+        console.log({ hostname });
+
+        if (
+            hostname.startsWith('localhost') ||
+            hostname.startsWith('127.0.0.1')
+        ) {
+            return;
         }
+
+        if (protocol.startsWith('https')) {
+            return;
+        }
+
+        window.location.href = window.location.href.replace('http', 'https');
     }, []);
 };
 
