52758cdba674f1451746c4865865a4955490df7e
jiho lee <optional.int@kakao.com>
2022-05-12 02:34:53 +0900

    chore: add edit user info page on settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { useState } from 'react';
+import ExtraInfo from './auth/extra-info';
 
-const Settings: NextPage = () => (
-    <Typography variant="h1" className="text-white">
-        Hello, JMT Monster!
-    </Typography>
-);
+const Settings: NextPage = () => {
+    const [editExtrainfo, setEditExtraInfo] = useState(false);
+    const completeEditExtraInfo = () => setEditExtraInfo(false);
+
+    return (
+        <div className="flex flex-col">
+            <button
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-10"
+                onClick={() => setEditExtraInfo(true)}
+            >
+                사용자 정보 수정
+            </button>
+            <button
+                type="button"
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+                로그아웃
+            </button>
+            {editExtrainfo && (
+                <ExtraInfo edit completeEdit={completeEditExtraInfo} />
+            )}
+        </div>
+    );
+};
 
 export default Settings;
