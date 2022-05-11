99749da463dbf9a4d3fb8ad698fcd434dce177a9
jiho lee <optional.int@kakao.com>
2022-05-12 02:41:48 +0900

    feat: add signOut

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,10 +1,36 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import ExtraInfo from './auth/extra-info';
+import useAuth from '../store/auth';
+import useSnackbarHandler from '../store/snackbar';
 
 const Settings: NextPage = () => {
+    const { user, signOut } = useAuth();
+    const router = useRouter();
+    const { setMessage: setSnackbarMessage } = useSnackbarHandler();
+
     const [editExtrainfo, setEditExtraInfo] = useState(false);
     const completeEditExtraInfo = () => setEditExtraInfo(false);
+
+    const [isSignOut, setIsSignOut] = useState(false);
+
+    const handleSignOut = () => {
+        setSnackbarMessage('info', '로그아웃 되었습니다.');
+        setIsSignOut(true);
+        signOut();
+        router.push('/');
+    };
+
+    useEffect(() => {
+        if (!user && !isSignOut) {
+            setSnackbarMessage(
+                'error',
+                '비로그인 사용자는 설정 페이지에 접근이 불가능합니다.'
+            );
+            router.push('/');
+        }
+    }, [user]);
 
     return (
         <div className="flex flex-col">
@@ -18,6 +44,7 @@ const Settings: NextPage = () => {
             <button
                 type="button"
                 className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                onClick={handleSignOut}
             >
                 로그아웃
             </button>
