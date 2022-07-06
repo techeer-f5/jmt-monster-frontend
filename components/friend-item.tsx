f2027a40fb4717750adb17f86e67dd6f05d57ceb
litsynp <nocte55is@gmail.com>
2022-07-06 21:25:42 +0900

    fix: fix nullable field in profile image alt

--- components/friend-item.tsx
+++ components/friend-item.tsx
@@ -5,7 +5,7 @@ const FriendItem = ({ friend }: { friend: Friend }) => {
         <div className="flex my-2 w-full items-center border-2 border-gray-800 text-gray-800 bg-white">
             <div className="w-20">
                 <img
-                    alt={`${friend.toUser.nickname} profile`}
+                    alt={`${friend.toUser.email} profile`}
                     src={
                         friend.toUser.imageUrl ??
                         'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
